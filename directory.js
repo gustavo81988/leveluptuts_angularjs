c08bed67bac7069e58e3049c9e3c1761513d3918
Gustavo Ramirez <gustavo.ramirez@ingeniandoweb.com>
2015-12-18 20:50:34 -0430

    Adding A Form Event

--- directory.js
+++ directory.js
@@ -1,9 +1,21 @@
 angular.module('directoryApp',[])
 	.controller('directoryController',function($scope){
+
 		$scope.list = [
 			{name:'Scott', age:29},
 			{name:'Ross', age:29},
 			{name:'Ben', age:29},
 			{name:'Courtney', age:29}
-		]
+		];
+
+		$scope.addPerson = function(){
+			$scope.list.push({
+				name : $scope.name,
+				age  : $scope.age
+			});
+			
+			$scope.name = "";
+			$scope.age  = "";
+		}
+
 	});
