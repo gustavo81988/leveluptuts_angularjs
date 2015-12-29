d6064a0d57f23d5e7a9bb4e8e910f4ea60e1ce4e
Gustavo Ramirez <gustavo.ramirez@ingeniandoweb.com>
2015-12-29 15:16:32 -0430

    Conditional Classes

--- directory.js
+++ directory.js
@@ -2,11 +2,14 @@ angular.module('directoryApp',[])
 	.controller('directoryController',function(){
 
 		var dirList = this;
+
+		dirList.toggle = false;
+
 		dirList.list = [
 			{name:'Scott', age:29},
 			{name:'Ross', age:31},
 			{name:'Ben', age:32},
-			{name:'Courtney', age:33}
+			{name:'Courtney', age:29}
 		];
 
 		dirList.addPerson = function(){
